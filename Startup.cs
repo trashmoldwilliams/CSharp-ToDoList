401797776d7a143085a0dc5c46d81799f9ab6678
Neil Larion <neil.larion@gmail.com>
2016-02-08 14:03:51 -0800

    Initial commit - setup base file structure

---
+++ Startup.cs
@@ -0,0 +1,22 @@
+using System.IO;
+using Microsoft.AspNet.Builder;
+using Nancy.Owin;
+using Nancy;
+
+namespace Parcels
+{
+  public class Startup
+  {
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseOwin(x => x.UseNancy());
+    }
+  }
+  public class CustomRootPathProvider : IRootPathProvider
+  {
+    public string GetRootPath()
+    {
+      return Directory.GetCurrentDirectory();
+    }
+  }
+}
